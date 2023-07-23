5d941cee175701361df037157d327ff4e336231b
Ojas Singh <ojas.singh02@gmail.com>
2023-07-23 12:19:42 -0400

    Bug fix: fixed hydration errors with theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
         Spotify web app
       </h1>
