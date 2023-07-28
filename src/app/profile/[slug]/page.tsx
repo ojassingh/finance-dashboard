837004dc888bb7184d41ea744ab7d6600a61ea3b
Ojas Singh <ojas.singh02@gmail.com>
2023-07-27 23:10:04 -0400

    Added function: fetchMusicInsights to get spotify user data

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -1,8 +1,35 @@
 'use client'
+import fetchMusicInsights from "@/functions/fetchMusicInsights";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
-function Profile({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+async function getData(accessToken: string) {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  const profileResponse = await axios.get('https://api.spotify.com/v1/me', {
+    headers: headers,
+  });
+  console.log(profileResponse.data)
+}
+
+
+
+function Profile({ params }: any) {
+
+  const [data, setData] = useState(null);
+  const accessToken = params.slug;
+  getData(accessToken)
+
+  // useEffect(() => {
+  //   const getData = async () => {
+      
+  //   }
+  //   getData()
+  // }, [])
+
+  return <div>My Post: {data}</div>
 }
 
 
