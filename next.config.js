079389f21f5c435341cea821a329a0795f54ee1d
Ojas Singh <ojas.singh02@gmail.com>
2023-08-02 23:13:39 -0400

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      },
+}
 
 module.exports = nextConfig
