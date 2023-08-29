334d0d865d6ad72a075444ebeaacaed06b9d90b9
Ojas Singh <ojas.singh02@gmail.com>
2023-08-29 07:39:41 +0530

    Bug fixes

--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card"
 
-import { cn } from "src/lib/utils"
+import { cn } from "../../lib/utils/utils"
 
 const HoverCard = HoverCardPrimitive.Root
 
