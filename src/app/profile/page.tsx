95b76d3b9b24f878468d451359d991102505801e
Ojas Singh <ojas.singh02@gmail.com>
2023-09-05 10:21:13 -0400

    final final changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/Navbar";
 
 export default function Profile(){
 
@@ -12,7 +14,7 @@ export default function Profile(){
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
     const [loading, setLoading] = useState(false)
-    const [data, setData] = useState(null);
+    const [data, setData] = useState<any>(null);
     const router = useRouter()
 
     async function getData(){
@@ -52,15 +54,44 @@ export default function Profile(){
       setLoading(false)
     }, [])
 
+    console.log(data)
+
 
     
     return (
-        <div>
+        <div className=" h-screen">
+          <Navbar/>
         {(loading || !auth) && <h1>Loading...</h1>}
 
         {auth && (
-            <div>
-                <h1>Hi Guys</h1>
+            <div className="grid place-content-cente gap-10 p-20">
+                <h1 className="text-2xl font-bold">Email: {email}</h1>
+                <h1 className="text-2xl font-bold">Name: {name}</h1>
+                <div className="text-2xl font-bold">
+                  <h1>
+                    Your articles:
+                  </h1>
+                  {data && <div>
+                    {data?.articles?.articles?.map((article: any) => {
+                      return(
+                        <Button variant="outline"><a target="_blank" id={article.id} href={article.url}>{article.title}</a></Button>
+                      )
+                    })}
+                  </div>}
+                </div>
+                
+                <div className="text-2xl font-bold">
+                  <h1>
+                      Your Indeces:
+                    </h1>
+                    {data && <div>
+                      {data?.indeces?.indeces?.map((index: any) => {
+                        return(
+                          <Button id={index.id} variant="outline">{index.index}</Button>
+                        )
+                      })}
+                    </div>}
+                </div>
             </div>
         )}
         </div>
